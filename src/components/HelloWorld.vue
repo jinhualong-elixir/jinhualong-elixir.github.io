0f84c83e61734439ab66201dfebebb30e6a16d67
USER-20220220DS\Administrator <bryan@elixiroftechnology.com>
2023-03-17 23:58:51 +0800

    beofre building

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,234 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello grid-container">
+    <div
+      v-for="i in 4"
+      :key="i"
+      :style="{ top: '0px', left: (i-1)*20+'%' }"
+      class="image-container"
+    >
+      <img :src="require(`@/assets/img/egg.png`)" class="image" />
+    </div>
+      <div
+      v-for="i in 5"
+      :key="i"
+      :style="{ bottom: '0px', left: (i-1)*20+'%' }"
+      class="image-container"
+    >
+      <img :src="require(`@/assets/img/heart.png`)" class="image" />
+    </div>
+    <el-image
+        class="bg"
+        :src="require(`@/assets/img/p${currentIndex+1}.jpg`)"
+        fit="fill"></el-image>
+    <header>
+        <div class="mirror">
+          <video ref="video" autoplay></video>
+        </div>
+    </header>
+    <main>
+      <div class="carl">
+        <el-carousel  ref="carl" @change="carouselChange" :interval="7000000" card-width="700" type="card" height="500px">
+          <el-carousel-item>
+            <SlideOne/>
+          </el-carousel-item>
+          <el-carousel-item>
+            <SlideTwo/>
+          </el-carousel-item>
+          <el-carousel-item>
+            <SlideThree/>
+          </el-carousel-item>
+          <el-carousel-item>
+            <SlideFour/>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </main>
+
+    <footer>Designed by Bryan • <a href="https://github.com/alexwidua/prototypes" target="_blank" rel="noopener">Github</a></footer>
+        
   </div>
 </template>
 
 <script>
+import SlideOne from './slides/SlideOne.vue'
+import SlideTwo from './slides/SlideOne.vue'
+import SlideThree from './slides/SlideOne.vue'
+import SlideFour from './slides/SlideOne.vue'
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      currentIndex: 0,
+      numIcons:8,
+      radius:150
+    }
+  },
+  components: {
+    SlideOne,
+    SlideTwo,
+    SlideThree,
+    SlideFour,
+  },
   props: {
     msg: String
+  },
+  created() {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } })
+      .then(stream => {
+        this.$refs.video.srcObject = stream;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  mounted() {
+    const carl = this.$refs.carl.$el
+    carl.addEventListener("wheel",this.handleWheel)
+  },
+  beforeUnmount() {
+    const carl = this.$refs.carl.$el
+    carl.removeEventListener("wheel",this.handleWheel)
+  },
+  methods:{
+    carouselChange(index){
+      this.currentIndex = index
+    },
+    handleWheel(e){
+      const delta = e.deltaY
+      const carl = this.$refs.carl
+      if(delta>0){
+        carl.next()
+      }else{
+        carl.prev()
+      }
+    }
   }
+
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.image-container {
+  position: absolute;
+  width: 13%;
+  height: 13%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 3;
+  margin: 45px 35px -35px 90px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.image {
+  max-width: 100%;
+  max-height: 100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.hello{height: 100%;}
+.grid-container{
+    display: grid;
+    grid-template-rows: 100px 1fr 50px;
+    grid-template-areas:
+    'header'
+    'main'
+    'footer';
 }
-a {
-  color: #42b983;
+
+
+.mirror { 
+  height: 100%;
+  width: 80px;
+  float: right;
+  border-radius: 50%;
+  overflow: hidden;  
+
+  margin-right: 80px
+}
+.mirror::after{
+  content: '';
+  position: absolute;
+  height: 80px;
+  width: 80px;
+  background-image: url('../assets/img/demon.png');
+  background-size: cover;
+  background-position: center;
+  right: 80px;
+}
+.carl {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+video {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+
+  transform: scaleX(-1);
+}
+
+
+.el-carousel__item {
+    width: 70%;
+    left: -10%;
+}
+
+.el-carousel__item:nth-child(3) {
+    background-image: url('../assets/img/p1.jpg');
+    background-size: cover;
+    background-position: center;
+}
+
+.el-carousel__item:nth-child(4) {
+    background-image: url('../assets/img/p2.jpg');
+    background-size: cover;
+    background-position: center;
+}
+
+.el-carousel__item:nth-child(5) {
+    background-image: url('../assets/img/p3.jpg');
+    background-size: cover;
+    background-position: center;
+}
+
+.el-carousel__item:nth-child(6) {
+    background-image: url('../assets/img/p4.jpg');
+    background-size: cover;
+    background-position: center;
+}
+
+header{
+  grid-area: header;
+  z-index: 1;
+
+  padding: 10px;
+}
+main{
+  grid-area: main;
+}
+footer{
+  grid-area: footer;
+  z-index: 1;
+}
+
+.bg{
+    position: absolute;
+    top: 0;
+    object-fit: fill;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    filter: blur(40px);
+}
+.bg::after{
+  content: "";
+  display: block;
+  background: rgba(0,0,0,0.5);
+  z-index:1;
 }
 </style>
---
+++ src/components/slides/SlideOne.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="page">
+      <h2>test test test</h2>
+      <p>test test testtest test test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlideOne',
+  data() {
+    return {
+      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,h2,h3,h4,h5,h6{
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+}
+p{
+  font-family: Verdana, sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #333333;
+}
+
+.page{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
